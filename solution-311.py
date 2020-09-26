baf87278639e95ac91206eaf1aa701cd86156f81
Alyx Faber <alyx.faber@student.csulb.edu>
2020-09-25 22:15:06 -0700

    sol311

--- solution-311.py
+++ solution-311.py
@@ -1,26 +1,16 @@
-# test
+# mpg calculator
 # gal = gallons used
 # mil = miles driven
 # mpg = miles per gallon
 
-gal1 = float(input("Enter the gallons used (-1 to end): "))
-mil1 = float(input("Enter the miles driven: "))
-mpg1 = mil1 / gal1
-print(f'The miles / gallon for this tank was {mpg1: ,.6f} \n')
-
-gal2 = float(input("Enter the gallons used (-1 to end): "))
-mil2 = float(input("Enter the miles driven: "))
-mpg2 = mil2 / gal2
-print(f'The miles / gallon for this tank was {mpg2: ,.6f} \n')
-
-gal3 = float(input("Enter the gallons used (-1 to end): "))
-mil3 = float(input("Enter the miles driven: "))
-mpg3 = mil3 / gal3
-print(f'The miles / gallon for this tank was {mpg3: ,.6f} \n')
-
-overallmpg = (mil1 + mil2 + mil3) / (gal1 + gal2 + gal3)
-
-gal4 = float(input("Enter the gallons used (-1 to end): "))
-print(f'The overall average miles / gallom was {overallmpg: ,.6f}')
-
-
+SENTINEL = -1
+result = []
+while True: 
+    gal = float(input("Enter the gallons used ( -1 to end ): "))
+    if gal == SENTINEL : break
+    mil = float(input("Enter the miles driven: "))
+    mpg = mil / gal 
+    print(f"The miles/gallon for this tank was {mpg: ,.6f}")
+    result.append(mpg) 
+    
+print("The overall average miles/gallon was",sum(result)/len(result))
